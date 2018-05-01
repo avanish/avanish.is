06df42e5e25bf076ad57ae0f06c1d22d9834a4fa
Avanish Shrestha <avanissh@gmail.com>
2018-05-01 12:18:36 +0700

    Added ssh-add in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+eval "$(ssh-agent -s)"
+
+expect << EOF
+  spawn ssh-add ~/.ssh/pixie_rsa
+  expect "Enter passphrase"
+  send $PASSPHRASE
+  expect eof
+EOF
+
 # eval "$(ssh-agent -s)" # Start ssh-agent cache
 # chmod 600 .travis/id_rsa # Allow read access to the private key
 # ssh-add .travis/id_rsa # Add the private key to SSH
