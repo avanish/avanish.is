77fadd1155cc693098dbc17ff1260f9f62962a3d
Avanish Shrestha <avanissh@gmail.com>
2018-11-12 16:27:28 +0700

    Fixed spawn

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@
 
 git remote add deploy ssh://abu@$IP:$DEPLOY_DIR
 expect << EOF
-  spwan git stash
+  spawn git stash
   expect eof
   spawn git push deploy master
   expect "Are you sure you want to continue connecting (yes/no)? "
